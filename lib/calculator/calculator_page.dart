c6c0fc13b26187f7ae9bd6fcb991a6c056764cda
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2023-04-30 13:58:26 +0530

    complete-calculator-design

--- lib/calculator/calculator_page.dart
+++ lib/calculator/calculator_page.dart
@@ -9,6 +9,112 @@ class CalculatorPage extends StatefulWidget {
 
 class _CalculatorPage extends State<CalculatorPage> {
   int _count = 0;
+  String _text = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: double.maxFinite,
+              // height: double.infinity,
+              padding: _text.isEmpty
+                  ? EdgeInsets.fromLTRB(
+                      0,
+                      MediaQuery.of(context).size.height * 0.1,
+                      MediaQuery.of(context).size.width * 0.4,
+                      0)
+                  : EdgeInsets.fromLTRB(
+                      0,
+                      MediaQuery.of(context).size.height * 0.3,
+                      MediaQuery.of(context).size.width * 0.05,
+                      0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(21),
+                  color: Colors.grey[800]),
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Text(
+                  _text.isEmpty ? "Calculator" : _text,
+                  style: const TextStyle(
+                      fontSize: 42,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 7,
+            child: GridView.builder(
+              itemCount: 20,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4),
+              itemBuilder: ((context, index) {
+                return ++_count % 4 == 0
+                    ? Padding(
+                        padding: index == 19
+                            ? const EdgeInsets.all(12.0)
+                            : const EdgeInsets.all(7.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 1),
+                            color: index == 19 ? Colors.blue : Colors.grey[800],
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              _text += _symbols[index];
+                              if (_text.contains("AC")) {
+                                _text = "";
+                              }
+                              setState(() {});
+                            },
+                            child: Center(
+                              child: Text(
+                                // _symbols.elementAt(_iter),,
+                                index % 4 == 0 ? "" : _symbols[index],
+                                // index.toString(),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : InkWell(
+                        onTap: () {
+                          _text += _symbols[index];
+                          if (_text.contains("AC")) {
+                            _text = "";
+                          }
+                          setState(() {});
+                        },
+                        child: Center(
+                          child: Text(
+                            _symbols[index],
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
+                      );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   List<String> _symbols = [
     "AC",
     "<=",
@@ -31,73 +137,4 @@ class _CalculatorPage extends State<CalculatorPage> {
     ".",
     "="
   ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Expanded(
-            // flex: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(21),
-                  color: Colors.grey[800]),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: GridView.builder(
-              itemCount: 20,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4),
-              itemBuilder: ((context, index) {
-                return ++_count % 4 == 0
-                    ? Padding(
-                        padding: index == 19
-                            ? const EdgeInsets.all(12.0)
-                            : const EdgeInsets.all(7.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(width: 1),
-                            color: index == 19 ? Colors.blue : Colors.grey[800],
-                            borderRadius: BorderRadius.circular(100),
-                          ),
-                          child: InkWell(
-                            child: Center(
-                              child: Text(
-                                // _symbols.elementAt(_iter),,
-                                index % 4 == 0
-                                    ? "1"
-                                    : _symbols[(index / 4).toInt() + 1],
-                                // index.toString(),
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    : InkWell(
-                        child: Center(
-                          child: Text(
-                            _symbols[index],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                            ),
-                          ),
-                        ),
-                      );
-              }),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
 }
